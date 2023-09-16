62ce0730bfaa85d0d8c3190069020b6ed7ef77a2
mhaddaou <mhaddaou@student.1337.ma>
2023-09-16 17:08:10 +0100

    update

--- dist/outils/ConnectDb.js
+++ dist/outils/ConnectDb.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
 const ConnectDb = () => {
-    mongoose_1.default.connect(`${process.env.CONNECT}`).then(() => { console.log('connect succesfully'); }).catch(() => { console.log('is not connect'); });
+    mongoose_1.default.connect(`mongodb+srv://mhaddaou:iQ8Ij9h9GgfBNZeC@cluster0.baz83mq.mongodb.net/mern?retryWrites=true&w=majority`).then(() => { console.log('connect succesfully'); }).catch(() => { console.log('is not connect'); });
 };
 exports.default = ConnectDb;
 //# sourceMappingURL=ConnectDb.js.map
--- src/outils/ConnectDb.ts
+++ src/outils/ConnectDb.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 
 
 const ConnectDb =  () => {
-     mongoose.connect(`${process.env.CONNECT}`).then(() =>{console.log('connect succesfully')}).catch(() =>{console.log('is not connect')});
+     mongoose.connect(`mongodb+srv://mhaddaou:iQ8Ij9h9GgfBNZeC@cluster0.baz83mq.mongodb.net/mern?retryWrites=true&w=majority`).then(() =>{console.log('connect succesfully')}).catch(() =>{console.log('is not connect')});
   };
 
 export default ConnectDb;
